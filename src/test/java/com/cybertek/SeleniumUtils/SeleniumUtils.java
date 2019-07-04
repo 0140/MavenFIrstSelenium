e8257bf6a57bcf6a41fcc9859f520f4527a89f0e
0140 <rustemov@yahoo.com>
2019-07-03 22:15:55 -0400

    adding day 7 notes Select dropdawn and Xpath

--- src/test/java/com/cybertek/SeleniumUtils/SeleniumUtils.java
+++ src/test/java/com/cybertek/SeleniumUtils/SeleniumUtils.java
@@ -49,6 +49,16 @@ public class SeleniumUtils {
             }
         }
     }
+
+    public static void verifyIsDisplayed(WebElement element){
+        if(element.isDisplayed()){
+            System.out.println("PASSED");
+            System.out.println(element.getText()+": is visible");
+        }else{
+            System.out.println("FAILED");
+            System.out.println(element.getText()+": is not visible!");
+        }
+    }
 }
 
 
